ce7edb40fb6a7f5a92e86d5ea8124ca74ec4af03
Leonardo Alfaya Fonseca <leo.alfaya@gmail.com>
2018-05-22 00:06:44 -0300

    fit_and_predict

--- machine_learning_1/src/classifica_buscas.py
+++ machine_learning_1/src/classifica_buscas.py
@@ -1,5 +1,7 @@
+from collections import Counter
 import pandas as pd
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.ensemble import AdaBoostClassifier
 
 df = pd.read_csv("busca.csv")
 X_df = df[['home', 'busca', 'logado']]
@@ -17,11 +19,26 @@ treino_marcacoes = Y[:tamanho_treino]
 teste_dados = X[-tamanho_teste:]
 teste_marcacoes = Y[-tamanho_teste:]
 
+
+def fit_and_predict(nome, modelo, treino_dados, treino_marcacoes,
+                    teste_dados, teste_marcacoes):
+    modelo.fit(treino_dados, treino_marcacoes)
+    resultado = modelo.predict(teste_dados)
+    acertos = list(filter(lambda x: x == True, resultado == teste_marcacoes))
+    margem_acertos = len(acertos) / len(teste_marcacoes) * 100
+
+    print('margem de acertos do {0}: {1}%'.format(nome, margem_acertos))
+
 modelo = MultinomialNB()
-modelo.fit(treino_dados, treino_marcacoes)
+fit_and_predict("MultinomialNB", modelo, treino_dados, treino_marcacoes,
+                    teste_dados, teste_marcacoes)
 
-resultado = modelo.predict(teste_dados)
-acertos = list(filter(lambda x: x == True, resultado == teste_marcacoes))
-margem_acertos = len(acertos) / len(teste_marcacoes) * 100
+modelo = AdaBoostClassifier()
+fit_and_predict("AdaBoostClassifier", modelo, treino_dados, treino_marcacoes,
+                    teste_dados, teste_marcacoes)
 
-print('margem de acertos: {}%'.format(margem_acertos))
+#eficácia do algoritmo que chuta tudo 1 ou 0
+taxa_acerto_base = max(Counter(teste_marcacoes).values()) * 100.0 / len(teste_marcacoes)
+
+print("Taxa de acerto base: {}%".format(taxa_acerto_base))
+print("Total de testes: {}".format(len(teste_dados)))
